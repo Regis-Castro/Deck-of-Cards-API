2198c5d44b1ea3136894861ac238eecb172e5ccd
Regis Castro <regis.gotardi@gmail.com>
2022-04-12 19:30:47 -0300

    Revisao CG

---
+++ src/js/index.js
@@ -0,0 +1,76 @@
+//minha versão
+let sacar = document.querySelector('#sacar');
+let retornar = document.querySelector('#retornar');
+
+document.getElementById('mostrar-carta').addEventListener('click', () => mostrarImagemEQtdRestante())
+
+document.getElementById('retornar-cartas').addEventListener('click', () => {
+  retornarCartasProDeck(id)
+  atualizarCartasRestantes(id)
+
+})
+
+let id = "" //funciona sem isso
+
+async function atribuirDeckID(){
+  id = await criarDeckID()
+}
+
+async function criarDeckID() {
+  const url = "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  const resposta = await fetch(url)
+  const json =  await resposta.json()
+  return json.deck_id
+}
+
+async function pegarImagemEQtdRestante(ID) {
+  const url = `http://deckofcardsapi.com/api/deck/${ID}/draw/?count=1`
+  const resposta = await fetch(url)
+  const json = await resposta.json()
+  console.log(json)
+  return [json.cards[0].image, json.remaining]
+}
+
+async function mostrarImagemEQtdRestante() {
+  const [imagem, restantes] = await pegarImagemEQtdRestante(id)
+  document.getElementById('carta').src = imagem
+  sacar.play()
+  // document.getElementById('carta').style.marginTop = '200px'
+  document.getElementById('restantes').innerHTML = `Cartas restantes: ${restantes}`
+
+  if (restantes == 0) {
+    document.getElementById('restantes').innerHTML = `O baralho terminou!`
+  }
+}
+
+async function retornarCartasProDeck(ID) {
+  const url = `http://deckofcardsapi.com/api/deck/${ID}/return/`
+  const resposta = await fetch(url)
+  const json = await resposta.json()
+  return json.remaining
+}
+
+async function atualizarCartasRestantes() {
+  const restantes = await retornarCartasProDeck(id)
+  document.getElementById('restantes').innerHTML = `Cartas restantes: ${restantes}`
+  document.getElementById('carta').src = "src/images/baralho.svg"
+  retornar.play()
+}
+
+
+
+//chama a função no primeiro carregamento do código e gera o id
+atribuirDeckID()
+
+
+
+
+
+
+
+
+
+
+
+
+
